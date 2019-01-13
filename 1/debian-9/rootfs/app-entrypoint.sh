c1c2a20942b8619f1c1d3e1f7d3aff2b0059b795
Stefaan <stefaanvermassen@users.noreply.github.com>
2019-01-13 19:30:21 +0100

    Update app-entrypoint.sh

--- 1/debian-9/rootfs/app-entrypoint.sh
+++ 1/debian-9/rootfs/app-entrypoint.sh
@@ -6,6 +6,8 @@
 print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "httpd" ]]; then
+  chmod 700 init.sh
+  chmod 700 create_metadata.sh
   . /init.sh
   ./create_metadata.sh
   nami_initialize apache php mysql-client libphp mediawiki
