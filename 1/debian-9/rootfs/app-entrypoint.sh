0415e8e0c082268b5c30a364212131744d759fe4
Stefaan <stefaanvermassen@users.noreply.github.com>
2019-01-13 18:49:31 +0100

    Update app-entrypoint.sh

--- 1/debian-9/rootfs/app-entrypoint.sh
+++ 1/debian-9/rootfs/app-entrypoint.sh
@@ -7,6 +7,7 @@ print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "httpd" ]]; then
   . /init.sh
+  ./create_metadata.sh
   nami_initialize apache php mysql-client libphp mediawiki
   info "Starting mediawiki... "
 fi
